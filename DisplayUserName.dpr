f8d948b2ce11c6dcd6bb587594941d0ea7a1d484
Bernhard Wendel <bwendel@gmx.de>
2017-05-06 15:32:10 +0200

    Adding Base Project Files

---
+++ DisplayUserName.dpr
@@ -0,0 +1,14 @@
+program DisplayUserName;
+
+uses
+  Vcl.Forms,
+  MainU in 'MainU.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
