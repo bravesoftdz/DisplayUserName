ebf71e83a035cbc97a463802d1e19e1cce602a19
Bernhard Wendel <bwendel@gmx.de>
2017-06-25 21:39:23 +0200

    Format Source File

--- MainU.pas
+++ MainU.pas
@@ -3,7 +3,8 @@ unit MainU;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
@@ -28,12 +29,12 @@ function CurrentUserName: String;
 var
   nSize: DWord;
 begin
- nSize := 1024;
- SetLength(Result, nSize);
- if GetUserName(PChar(Result), nSize) then
-   SetLength(Result, nSize-1)
- else
-   RaiseLastOSError;
+  nSize := 1024;
+  SetLength(Result, nSize);
+  if GetUserName(PChar(Result), nSize) then
+    SetLength(Result, nSize - 1)
+  else
+    RaiseLastOSError;
 end;
 
 procedure TFrmMain.FormCreate(Sender: TObject);
